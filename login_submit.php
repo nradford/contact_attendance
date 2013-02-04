b594b2edabcb56957cb06dbf4355a8aa2ab47e67
Nick Radford <nick@nickradford.net>
2013-02-03 21:48:25 -0600

    Moved version 1 files to archive directory and added CodeIgniter 2.1.3 files.

--- login_submit.php
+++
@@ -1,9 +0,0 @@
-<?session_start();
-if($_POST['username'] == "nick" && $_POST['password']== "nick"){
-	$_SESSION['logged_in']="1";
-	header("Location: check_in.php");
-}else{
-	$notification="Incorrect username or password entered.";
-	require_once 'index.php';
-}
-?>
