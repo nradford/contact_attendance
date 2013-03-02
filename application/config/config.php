b7275f21877e43ee2caa67194258b72379ad2b6c
Nick Radford <nick@nickradford.net>
2013-03-02 14:19:32 -0600

    Updated base path/url after making changes to Apache
    Updated the base path in .htaccess and the base_url setting in config.php after setting up Apache to use Named Virtual Hosts

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.com/contact_attendance/';
+$config['base_url']	= 'http://dev-ca.com/';
 
 /*
 |--------------------------------------------------------------------------
