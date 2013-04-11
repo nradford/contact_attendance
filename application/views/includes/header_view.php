10a4b0ff15f65d464b6eb82120cb974d6eed6934
Nick Radford <nick@nickradford.net>
2013-04-10 22:01:25 -0500

    Updated the navigation to show/hide different fields on different screen sizes as well as added comments around js and css includes for build scripts

--- application/views/includes/header_view.php
+++ application/views/includes/header_view.php
@@ -8,20 +8,29 @@
 	<title>Kidz Rock Check-In/Check-Out</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
+    <!-- //-beg- concat_css -->
     <link rel="stylesheet" href="<?php print base_url();?>css/styles.css" />
     <link rel="stylesheet" href="<?php print base_url();?>css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php print base_url();?>css/footable-0.1.css">
     <link rel="stylesheet" href="<?php print base_url();?>css/bootstrap-editable.css">
+    <!-- //-end- concat_css -->
 
 	<script src="<?print base_url();?>js/modernizr.custom.2.6.2.js"></script>
-    
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script>window.jQuery || document.write('<script src="<?php print base_url()."js/jquery-1.9.1.min.js";?>"><\/script>')</script>
+
+    <!-- //-beg- concat_js -->
     <script src="<?php print base_url();?>js/moment.min.js"></script>
     <script src="<?php print base_url();?>js/bootstrap.min.js"></script>
     <script src="<?php print base_url();?>js/bootstrap-editable.min.js"></script>
     <script src="<?php print base_url();?>js/footable-0.1.js"></script>
+    <script src="<?php print base_url();?>js/footable.filter.js"></script>
     <script src="<?php print base_url();?>js/jquery.ziptastic.js"></script>    
+    <!-- //-end- concat_js -->
+    
+    <?php if($this->uri->segment(1) == "reports"){?>
+        <script src="<?php print base_url();?>js/footable.sortable.js"></script><?php
+    }?>
 </head>
 <body>
 
@@ -31,20 +40,23 @@
             <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
                 <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
             </button>
-            <a class="brand" href="#">Check-In/Out</a>
+            <!-- <a class="brand" href="#">Check-In/Out</a> -->
+            
+            <ul class="nav">
+                <li<?php if($this->uri->segment(1) == "check_in")print ' class="active"';?>><a href="<?php print base_url();?>">Check-In</a></li>
+                <li<?php if($this->uri->segment(1) == "check_out")print ' class="active"';?>><a href="<?php print base_url();?>check_out">Check-Out</a></li>
+                <li<?php if($this->uri->segment(1) == "contacts")print ' class="active"';?>><a href="<?php print base_url();?>contacts">Kids</a></li>
+            </ul>
+
             <div class="nav-collapse collapse">
-                <p class="navbar-text pull-right"><?php
-                    if($this->session->userdata('account_id') > 0){?>
-                        <a href="<?php print base_url();?>login/logout" class="navbar-link">Log Out</a><?php
-                    }else{?>
-                        <?php
-                    }?>
-                </p>
                 <ul class="nav">
-                    <li<?php if($this->uri->segment(1) == "check_in")print ' class="active"';?>><a href="<?php print base_url();?>">Check-In</a></li>
                     <li<?php if($this->uri->segment(1) == "reports")print ' class="active"';?>><a href="<?php print base_url();?>reports">Reports</a></li>
-                    <li<?php if($this->uri->segment(1) == "contacts")print ' class="active"';?>><a href="<?php print base_url();?>contacts">Kids</a></li>
                 </ul>
+
+                <?php
+                if($this->session->userdata('account_id') > 0){?>
+                    <ul class="nav pull-right"><li><a href="<?php print base_url();?>login/logout" class="">Log Out</a></li></ul><?php
+                }?>
             </div><!-- .nav-collapse -->
         </div>
     </div>
