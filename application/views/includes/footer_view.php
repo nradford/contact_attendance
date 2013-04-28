60dc5cfb35a431e97a63f16d6ca8d6242aa3316c
Nick Radford <nick@nickradford.net>
2013-04-27 19:15:44 -0500

    Removed the footer copy.

--- application/views/includes/footer_view.php
+++ application/views/includes/footer_view.php
@@ -1,6 +1,6 @@
 
     <div class="row-fluid">
-        <footer class="span12">&copy; Solid Rock Family Church <?php print date("Y");?></footer>
+        <!-- <footer class="span12">&copy; Solid Rock Family Church <?php print date("Y");?></footer> -->
     </div>
 </div> <!--! end of .container-fluid -->
 
