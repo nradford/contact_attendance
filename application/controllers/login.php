9448260909bb0d7f791adb28cfd379bc1affdfd8
Nick Radford <nick@nickradford.net>
2013-05-10 17:00:00 -0500

    Changed the session from userdata to flashdata

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -39,8 +39,8 @@ class Login extends CI_Controller{
 			$this->session->set_userdata($data);
             redirect(base_url().'check_in');
 		}else{// incorrect email or password
-			$this->session->set_userdata('email', $this->input->post('email'));
-			$this->session->set_userdata('error', 'Incorrect email or password');
+			$this->session->set_flashdata('email', $this->input->post('email'));
+			$this->session->set_flashdata('error', 'Incorrect email or password');
             $this->index();
 		}
 	}
