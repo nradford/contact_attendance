6849e31220271e82e4df91b5cdd03e49fe03b50e
Nick Radford <nick@nickradford.net>
2014-03-09 09:54:34 -0500

    Updated email config file
    Signed-off-by: Nick Radford <nick@nickradford.net>

--- application/config/email.php
+++ application/config/email.php
@@ -1,11 +1,11 @@
-<?
+<?php
 $config = array();
 $config["mailtype"] = "html";
 $config["useragent"] = "none";
 $config["protocol"] = "smtp";
 $config["smtp_host"] = "smtp.mandrillapp.com";
-$config["smtp_user"] = "nick@solidrockfamilychurch.org";
-$config["smtp_pass"] = "1ZPly6nZhF_qrLavRHz_7A";
+$config["smtp_user"] = "";
+$config["smtp_pass"] = "";
 $config["smtp_port"] = "587";
 $config["smtp_timeout"] = "5";
 $config["newline"] = "\r\n";
